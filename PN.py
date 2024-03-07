27c151999162bfb402215170d2d367993fd42b56
HebaSalem <puupiiiii@gmail.com>
2024-03-07 23:12:43 +0100

    upload all files

---
+++ PN.py
@@ -0,0 +1,12 @@
+# due to an error occurs in Auth0 
+# I used these links 
+# https://127.0.0.1:8100/login  ==> Application Login URI
+# https://127.0.0.1:8100/logout ==> Application Logout URI
+# the login page doesn't open with this link
+# https://heba-salem.eu.auth0.com/authorize?audience=CoffeeShop&response_type=token&client_id=BRQ0yfJ5IlnbqxMln0lVDrsQXXSStRFT&redirect_uri=https://http://localhost:8100
+#  can you provide me with the login & logout code
+
+# also Auth0 requires https instead of http
+
+# this is the link of the project on GitHub
+# https://github.com/HebaMak/Coffee_Shop
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,20 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'heba-salem.eu.auth0.com', // the auth0 domain prefix
+    audience: 'CoffeeShop', // the audience set for the auth0 app
+    clientId: 'BRQ0yfJ5IlnbqxMln0lVDrsQXXSStRFT', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
+
+
+// due to an error occurs in Auth0 
+// I used these links 
+// https://127.0.0.1:8100/login  ==> Application Login URI
+// https://127.0.0.1:8100/logout ==> Application Logout URI
+// the login page doesn't open with this link, 
+// https://heba-salem.eu.auth0.com/authorize?audience=CoffeeShop&response_type=token&client_id=BRQ0yfJ5IlnbqxMln0lVDrsQXXSStRFT&redirect_uri=https://http://localhost:8100
+// can you provide me with the login & logout code
+
+// also Auth0 requires https instead of http
